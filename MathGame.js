3f116901fe03f014297b849798c9343b4dfa426a
Marcia Furtado <58325804+marciafurtado@users.noreply.github.com>
2020-01-29 14:22:41 +0100

    Adding the exercise code

---
+++ MathGame.js
@@ -0,0 +1,24 @@
+function mathGame(arrLength){
+    var gameArray = [];
+
+    for (var i = 1; i <= arrLength; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            gameArray.push("FizzBuzz");
+        } else if (i % 3 === 0){
+            gameArray.push("Fizz");
+        } else if (i % 5 === 0){
+            gameArray.push("Buzz");
+        } else {
+            gameArray.push(i);
+        }
+    }
+   
+return gameArray; 
+}
+
+
+mathGame(100)
+
+
+
+  
